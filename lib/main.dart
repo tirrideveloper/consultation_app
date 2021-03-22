a69e72decd4e87ce1244a173b0e74b4849521b00
Halil <halilbolat.hb@gmail.com>
2021-03-22 21:15:55 +0300

    Ben yaptim abi

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-          body: Center(child: Text("Hello")),
+          body: Center(child: Text("Halil Ben Sa")),
         ));
   }
 }
